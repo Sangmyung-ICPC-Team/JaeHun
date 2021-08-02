58a7b93ff8581a5bccc78fbd92e810d584eb8a3e
Chu-Jae-Hun <79078140+Chu-Jae-Hun@users.noreply.github.com>
2021-08-02 16:42:06 +0900

    Create Baekjoon - 1654 (랜선 자르기).py

---
+++ Baekjoon training/Python/Baekjoon - 1654 (랜선 자르기).py
@@ -0,0 +1,20 @@
+k, n = map(int, input().split())
+arr = []
+for i in range(k):
+    arr.append(int(input()))
+
+start = 1
+end = max(arr)
+
+result = 0
+while start <= end:
+    total = 0
+    mid = (start + end) // 2
+    for i in arr:
+            total += i // mid
+    if total < n:
+        end = mid - 1
+    else:
+        result = mid
+        start = mid + 1
+print(result)
