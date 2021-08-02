1556683aa32bdf83fcb78a5b110675cc37d3a3ae
Chu-Jae-Hun <79078140+Chu-Jae-Hun@users.noreply.github.com>
2021-08-02 14:28:47 +0900

    Create Baekjoon - 2960 (에라토스테네스의 체).py

---
+++ Baekjoon training/Python/Baekjoon - 2960 (에라토스테네스의 체).py
@@ -0,0 +1,15 @@
+import sys
+input = sys.stdin.readline
+
+n, k = map(int, input().split())
+arr = [True for i in range(n+1)]
+
+cnt = 0
+for i in range(2, len(arr)+1):
+    for j in range(i, n+1, i):
+        if arr[j] == True:
+            arr[j] = False
+            cnt += 1
+            if cnt == k:
+                print(j)
+                break
