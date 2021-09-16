b096bd9ef09cf8c8579ab4b0a50e4e8f3e390bbf
Chu-Jae-Hun <79078140+Chu-Jae-Hun@users.noreply.github.com>
2021-09-16 14:58:55 +0900

    Create Baekjoon - 11728 (배열 합치기).py

---
+++ Baekjoon training/Python/Baekjoon - 11728 (배열 합치기).py
@@ -0,0 +1,9 @@
+import sys
+input = sys.stdin.readline
+
+a, b = map(int, input().split())
+a_list = list(map(int, input().split()))
+b_list = list(map(int, input().split()))
+answer_list = a_list + b_list
+answer = ' '.join(map(str, sorted(answer_list)))
+print(answer)
